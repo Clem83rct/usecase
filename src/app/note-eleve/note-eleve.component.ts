5595c607d96f7d0b11ae2f12bad1747f26aff806
adrilafosse <81637698+adrilafosse@users.noreply.github.com>
2023-03-16 11:00:46 +0100

    le site marche pour donner une note a un eleve

--- src/app/note-eleve/note-eleve.component.ts
+++ src/app/note-eleve/note-eleve.component.ts
@@ -2,12 +2,27 @@ import { Component } from '@angular/core';
 
 
 
+
+
 @Component({
   selector: 'app-note-eleve',
   templateUrl: './note-eleve.component.html',
-  styleUrls: ['./note-eleve.component.css']
+  styleUrls: ['./note-eleve.component.css'],
 })
 export class NoteEleveComponent {
+  note!: number;
+  resultat: number | null = null;
+  erreur: string | null = null;
+
+  valider() {
+    if (this.note >= 0 && this.note <= 20) {
+      this.resultat = this.note;
+      this.erreur = null;
+    } else {
+      this.resultat = null;
+      this.erreur = 'La note doit être comprise entre 0 et 20 inclus.';
+    }
+  }
 
 
 }
